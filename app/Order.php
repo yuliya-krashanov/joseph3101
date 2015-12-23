597371af3fc6917c844e59daedd429467edd4f65
Yuliya Krashanovskaya <yuliya-krashanov@yandex.ua>
2015-12-24 00:24:40 +0200

    order create - start

--- app/Order.php
+++ app/Order.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $table = 'orders';
+
+
+    public function user()
+    {
+    	return $this->belongsTo('App/User');
+    }
+
+    public function delivery_method()
+    {
+    	return $this->belongsTo('App/DeliveryMethod');
+    }
+
+    public function payment_method()
+    {
+    	return $this->belongsTo('App/PaymentMethod');
+    }
 }
---
+++ resources/views/pages/thankyou.blade.php
@@ -0,0 +1,64 @@
+@extends('app')
+
+@section('page-title', 'Thank you!')
+
+@section('header')
+    <body class="thank_you_page">
+    @parent
+@endsection  
+	
+@section('content')    
+    <section class="titlearea">
+    	<div class="container">
+        	<div class="row">
+            	<h3>&nbsp;</h3><br><br>
+            </div>
+        </div>
+    </section>
+    
+    <section class="contentarea">
+    	<div class="container">
+        	<div class="row">
+            	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="col-md-2"></div>
+                <div class="col-md-8">
+                	<div class="joseph">
+                        <!------>
+                    <div class="visible-xs">
+                        	<div class="comment">
+                    			<h3 class="thank">Thank you<br>for your order,<br>your order number is</h3>
+                        		<h2>{{ $id }}</h2>
+                        		<h3>Joseph will be<br>happy to meet<br>you soon...</h3>
+                            </div>
+                        </div>
+                        <!------>
+            			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                        	<div class="joseph-img">
+                            	<img src="img/joseph-1.png" class="img-responsive">
+                            </div>
+                        </div>
+                        <!------>
+            			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 mobile-invisible">
+                        	<div class="comment">
+                    			<h3 class="thank">Thank you<br>for your order,<br>your order number is</h3>
+                        		<h2>{{ $id }}</h2>
+                        		<h3>Joseph will be<br>happy to meet<br>you soon...</h3>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                    
+                    <div class="col-md-2"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+@endsection
+
+
+@section('footer')
+  <footer class="thank_you_page">    
+      @parent    
+  </footer>
+@endsection 
